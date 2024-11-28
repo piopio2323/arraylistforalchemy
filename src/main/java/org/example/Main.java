1626d9422ee4a8fa45fc7ea023b1eb9bd0aac331
David Houston <djhouston2020@outlook.com>
2024-11-28 14:12:24 +0000

    Initial commit with ArrayList example

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> items = new ArrayList<>();
+        items.add("Apple");
+        items.add("Banana");
+        items.add("Cherry");
+
+        System.out.println("ArrayList: " + items);
+    }
+}
